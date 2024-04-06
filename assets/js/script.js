83ce1ace9f34abf48c9129b407c5591cb2baf097
gera9999 <gabenavented@gmail.com>
2024-04-06 09:55:40 -0300

    se corrige smooth scroll y detalles. No es posbile visualizar bien en git pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,11 @@
-$(function () {
-
-    $("a").click(function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-
-            var slide = this.hash;
-
-            $("html, body").animate({
-                scrollTop: $(slide).offset().top
-            }, 800, function () {
-                window.location.hash = slide;
-            });
-        }
-    });
-
-    $(function () {
-        $('[data-toggle="tooltip"]').tooltip()
-    })
-
-});
+ $("a.nav-link").on('click', function (event) {
+     if (this.hash !== "") {
+         event.preventDefault();
+         var hash = this.hash;
+         $('html, body').animate({
+             scrollTop: $(hash).offset().top
+         }, 800, function () {
+             window.location.hash = hash;
+         });
+     }
+ });
